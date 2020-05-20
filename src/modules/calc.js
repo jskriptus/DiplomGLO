89b73ff62379b6c3c5492eb6e52cdad88d265f26
jskriptus <skriptus@bk.ru>
2020-05-21 01:39:27 +0300

    прилипание бургер меню при скроле

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -6,44 +6,46 @@ const calc = () => {
     let discount = 1;
 
     const addPrice = () => {
-        if (time && promocode && price) {
-            let totalPrice = 0;
-            time.forEach(item => {
-                if (item.hasAttribute('checked')) {
+
+        let totalPrice = 0;
+        time.forEach(item => {
+            if (item.hasAttribute('checked')) {
+                totalPrice = Math.floor(2999 * discount);
+                price.textContent = totalPrice;
+            }
+            item.addEventListener('click', () => {
+                const target = event.target;
+
+                if (target.value === '1') {
                     totalPrice = Math.floor(2999 * discount);
                     price.textContent = totalPrice;
+                } else if (target.value === '6') {
+                    totalPrice = Math.floor(9900 * discount);
+                    price.textContent = totalPrice;
+                } else if (target.value === '9') {
+                    totalPrice = Math.floor(13900 * discount);
+                    price.textContent = totalPrice;
+                } else if (target.value === '12') {
+                    totalPrice = Math.floor(19900 * discount);
+                    price.textContent = totalPrice;
                 }
-                item.addEventListener('click', () => {
-                    const target = event.target;
-
-                    if (target.value === '1') {
-                        totalPrice = Math.floor(2999 * discount);
-                        price.textContent = totalPrice;
-                    } else if (target.value === '6') {
-                        totalPrice = Math.floor(9900 * discount);
-                        price.textContent = totalPrice;
-                    } else if (target.value === '9') {
-                        totalPrice = Math.floor(13900 * discount);
-                        price.textContent = totalPrice;
-                    } else if (target.value === '12') {
-                        totalPrice = Math.floor(19900 * discount);
-                        price.textContent = totalPrice;
-                    }
-                })
             })
-        }
+        })
+
     }
 
-    addPrice();
+    if (time && promocode && price) {
+        addPrice();
 
-    promocode.addEventListener('input', () => {
-        const target = event.target;
-        if (target.value === 'ТЕЛО2019') {
-            discount = 0.70;
-            addPrice();
-        }
+        promocode.addEventListener('input', () => {
+            const target = event.target;
+            if (target.value === 'ТЕЛО2019') {
+                discount = 0.70;
+                addPrice();
+            }
 
-    })
+        })
+    }
 }
 
 export default calc;
--- src/modules/topMenu.js
+++ src/modules/topMenu.js
@@ -1,17 +1,56 @@
 const topMenu = () => {
-    const hiddenSmall =  document.querySelector('.hidden-small');
-    const hiddenLarge = document.querySelector('.hidden-large');
+    const topMenu = document.querySelector('.top-menu');
+    const hiddenSmall = document.querySelector('.hidden-small');
+    const burgerMenu = document.querySelector('.menu-button');
+    const popupMenu = document.querySelector('.popup-menu');
+
+    if (document.documentElement.clientWidth < 768) {
+        hiddenSmall.style.display = 'none';
+        burgerMenu.style.display = 'block';
+    }
+
+    window.addEventListener('scroll', () => {
+        const scrollTop = document.documentElement.scrollTop;
+
+        if (scrollTop > topMenu.offsetTop) {
+            topMenu.classList.add('menu-fix');
+            return;
+        } else {
+            topMenu.classList.remove('menu-fix');
+            return;
+        }
+    });
 
     window.addEventListener('resize', () => {
         if (document.documentElement.clientWidth < 768) {
             hiddenSmall.style.display = 'none';
-            hiddenLarge.style.display = 'block';
+            burgerMenu.style.display = 'block';
+            return;
         } else {
             hiddenSmall.style.display = 'flex';
-            hiddenLarge.style.display = 'none';
+            burgerMenu.style.display = 'none';
+            return;
         }
-    })
-    
+    });
+
+    burgerMenu.addEventListener('click', () => {
+        const target = event.target;
+        if (target.matches('img')) {
+            popupMenu.style.display = 'flex';
+        } else {
+            return;
+        }
+    });
+
+    popupMenu.addEventListener('click', () => {
+        const target = event.target;
+        if (target.matches('img') || target.matches('a')) {
+            popupMenu.style.display = "none";
+        } else {
+            return;
+        }
+    });
+
 };
 
 export default topMenu;
